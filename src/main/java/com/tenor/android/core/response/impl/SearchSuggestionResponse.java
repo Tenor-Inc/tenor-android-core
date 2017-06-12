1a0a4b4655323d74da7b933d7483409538a24bb3
Jiyang Liu <jiyang@tenor.co>
2017-06-12 14:20:07 -0700

    [ANDROID-2002] added and implemented the SearchSuggestionResponse;

---
+++ src/main/java/com/tenor/android/core/response/impl/SearchSuggestionResponse.java
@@ -0,0 +1,24 @@
+package com.tenor.android.core.response.impl;
+
+import android.support.annotation.NonNull;
+
+import com.tenor.android.core.model.impl.PivotResult;
+import com.tenor.android.core.response.AbstractResponse;
+import com.tenor.android.core.util.AbstractListUtils;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * The search suggestion response
+ */
+public class SearchSuggestionResponse extends AbstractResponse {
+
+    private static final long serialVersionUID = 8046525237691607393L;
+    private List<String> results;
+
+    @NonNull
+    public List<String> getResults() {
+        return !AbstractListUtils.isEmpty(results) ? results : Collections.<String>emptyList();
+    }
+}
