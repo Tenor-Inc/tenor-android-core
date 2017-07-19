993b463527025dc6a66f1e2abaada606ba3771bb
Jiyang Liu <jiyang@tenor.co>
2017-07-18 17:40:35 -0700

    [ANDROID-2174] moved CallStub to `core` module;

---
+++ src/main/java/com/tenor/android/core/network/impl/CallStub.java
@@ -0,0 +1,49 @@
+package com.tenor.android.core.network.impl;
+
+import java.io.IOException;
+
+import okhttp3.Request;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CallStub<T> implements Call<T> {
+    public CallStub() {
+    }
+
+    @Override
+    public Response<T> execute() throws IOException {
+        return null;
+    }
+
+    @Override
+    public void enqueue(Callback<T> callback) {
+
+    }
+
+    @Override
+    public boolean isExecuted() {
+        return false;
+    }
+
+    @Override
+    public void cancel() {
+
+    }
+
+    @Override
+    public boolean isCanceled() {
+        return false;
+    }
+
+    @SuppressWarnings("CloneDoesntCallSuperClone")
+    @Override
+    public Call<T> clone() {
+        return null;
+    }
+
+    @Override
+    public Request request() {
+        return null;
+    }
+}
